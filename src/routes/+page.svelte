53c8422b98c85c728cf3891437fc959688f82733
null <null>
2024-01-22 19:45:38 +0700

    v1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import { emoji } from './emoji'
-
 	type State = 'start' | 'playing' | 'paused' | 'won' | 'lost'
 
 	let state: State = 'start'
@@ -96,6 +95,8 @@
 	$: selected.length === 2 && matchCards()
 	$: maxMatches === matches.length && gameWon()
 	$: time === 0 && gameLost()
+
+
 </script>
 
 <svelte:window on:keydown={pauseGame} />
@@ -110,9 +111,15 @@
 {/if}
 
 {#if state === 'playing'}
-	<h1 class="timer" class:pulse={time <= 10}>
-		{time}
-	</h1>
+	<div class="progress">
+		<h1 class="timer" class:pulse={time <= 10}>
+			{time}
+		</h1>
+
+		<div class="timerround" class:pulseround={time <= 10} style="--duration: 60;--size: 150;">
+			<div class="mask"></div>
+		</div>
+	</div>
 
 	<div class="matches">
 		{#each matches as card}
@@ -150,26 +157,48 @@
 {#if state === 'won'}
 	<h1>You win! 🎉</h1>
 	<button on:click={() => (state = 'playing')}>Play again</button>
+	
+	//FIREWORK
+	<div class="pyro">
+		<div class="before"></div>
+		<div class="after"></div>
+	</div>
+
 {/if}
 
 <style>
 	.cards {
 		display: grid;
 		grid-template-columns: repeat(5, 1fr);
-		gap: 0.4rem;
+		gap: 1rem;
+		background-color: var(--bg-2);
+		padding: 5rem;
+		//border-radius: 1rem;
+		
+
+		background: linear-gradient(45deg, var(--primary) 0%, var(--secondary) 100%);
+		animation: morph 8s ease-in-out infinite;
+		border-radius: 60% 40% 30% 70% / 60% 30% 70% 40%;
+		transition: all 1s ease-in-out;
+z-index: 5;
+		
 	}
 
 	.card {
 		height: 140px;
 		width: 140px;
 		font-size: 4rem;
-		background-color: var(--bg-2);
+		background-color: var(--bg-3);
 		transition: rotate 0.3s ease-out;
 		transform-style: preserve-3d;
+		
+		border: 1px solid rgba(255, 255, 255, 0.2);
 
 		&.selected {
-			border: 4px solid var(--border);
+			//border: 4px solid var(--border);
+			box-shadow: 0 0 10px 5px var(--border), 0 0 10px var(--border), 0 0 15px var(--border), 0 0 20px var(--border);
 		}
+
 
 		&.flip {
 			rotate: y 180deg;
@@ -198,13 +227,46 @@
 		font-size: 3rem;
 	}
 
+	.progress{
+		display: flex;
+		justify-content: center;		
+	}
 	.timer {
 		transition: color 0.3s ease;
+		width:80px;
+		height:80px;
 	}
 
+	.timerround {
+		background: -webkit-linear-gradient(left, skyBlue 50%, #eee 50%);
+		border-radius: 100%;
+		height: calc(var(--size) * 1px);
+		width: calc(var(--size) * 1px);
+		margin-top: -30px;
+		margin-left: -115px;
+		-webkit-animation: timeround calc(var(--duration) * 1s) steps(1000, start) infinite;
+		-webkit-mask: radial-gradient(transparent 50%,#000 50%);
+		mask: radial-gradient(transparent 50%,#000 50%);
+	}
+	.mask {
+		border-radius: 100% 0 0 100% / 50% 0 0 50%;
+		height: 100%;
+		left: 0;
+		position: absolute;
+		top: 0;
+		width: 50%;
+		-webkit-animation: mask calc(var(--duration) * 1s) steps(500, start) infinite;
+		-webkit-transform-origin: 100% 50%;
+	}
+	
 	.pulse {
 		color: var(--pulse);
+		width:80px;
+		height:80px;
 		animation: pulse 1s infinite ease;
+	}
+	.pulseround {
+		display:none;
 	}
 
 	@keyframes pulse {
@@ -212,4 +274,47 @@
 			scale: 1.4;
 		}
 	}
+
+	@keyframes morph {
+		0% {
+			border-radius:  60% 40% 30% 70% / 60% 30% 70% 40%;
+			background: linear-gradient(45deg, var(--primary) 0%, var(--secondary) 100%);
+		} 
+		
+		50% {
+			border-radius:  30% 60% 70% 40% / 50% 60% 30% 60%;
+			background: linear-gradient(45deg, var(--third) 0%, var(--secondary) 100%);
+		}
+		
+		100% {
+			border-radius:  60% 40% 30% 70% / 60% 30% 70% 40%;
+			background: linear-gradient(45deg, var(--primary) 0%, var(--secondary) 100%);
+		} 
+	}
+
+	@-webkit-keyframes timeround {
+		100% {
+			-webkit-transform: rotate(360deg);
+		}
+	}
+	@-webkit-keyframes mask {
+		0% {
+			background: #eee;
+			-webkit-transform: rotate(0deg);
+		}
+		50% {
+			background: #eee;
+			-webkit-transform: rotate(-180deg);
+		}
+		50.01% {
+			background: skyBlue;
+			-webkit-transform: rotate(0deg);
+		}
+		100% {
+			background: skyBlue;
+			-webkit-transform: rotate(-180deg);
+		}
+	}
+	
+
 </style>
